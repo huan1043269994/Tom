5833ee18d2a4793f5a75fe7bffec0b5d8cda8e0b
指尖泛出的繁华 <1043269994@qq.com>
2016-08-25 10:55:15 +0800

    Update ie_other.js

--- jQuery/ie_other.js
+++ jQuery/ie_other.js
@@ -1,4 +1,6 @@
+//1.兼容所有浏览器，ie和其他的事件调用。
 var eventUtil = {
+            //增加事件
             addHandler: function (element, type, handler) {
               if(element.addEventListener){
                 element.addEventListener(type, handler, false);
@@ -9,7 +11,7 @@ var eventUtil = {
                 element['on' + type] = handler;
               }
             },
-            
+            //删除事件
             removeHandler: function (element, type, handler) {
               if(element.removeEventListener){
                 element.removeEventListener(type, handler, false);
@@ -19,5 +21,43 @@ var eventUtil = {
                 element.onclick === element['onclick'];
                 element['on' + type] = null;
               }
+            },
+            //事件类型
+            getType: function (event){
+                        return event.type;
+            },
+            //事件目标
+            getElement: function (event) {
+                        return event.target || event.srcElement;
+            },
+            //事件
+            getEvent: function (event) {
+                        return event?event:window.event;           
+            },
+            //停止默认行为
+            preventDefault: function (event) {
+                        if(event.preventDefault){
+                         event.preventDefault();           
+                        } else {
+                         event.returnValue = false;
+                        }
+                       
+            },
+            //阻止冒泡
+            stopPropagation: function (event) {
+             if(event.stopPropagation) {
+                         event.stopPropagation();
+             } else {
+                         event.cancelBubble = true;
+             }
             }
+            
           }
+//测试
+            eventUtil.addHandler(go,'click',function (e) {
+                        e = eventUtil.getEvent(e);
+                        alert(eventUtil.getElement(e).nodeName);
+                        eventUtil.preventDefault(e);
+                        eventUtil.stopPropagation(e);
+            });
+            
