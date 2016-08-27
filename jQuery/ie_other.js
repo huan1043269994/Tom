c7d093ee3d5b24c11a9b8be648689fd946aec87a
指尖泛出的繁华 <1043269994@qq.com>
2016-08-27 14:24:14 +0800

    Update ie_other.js

--- jQuery/ie_other.js
+++ jQuery/ie_other.js
@@ -61,3 +61,12 @@ var eventUtil = {
                         eventUtil.stopPropagation(e);
             });
             
+            //返回本元素样式，不包括边框,width,height,font-size,backgroundColor,color,等等
+            function getStyle(obj, attr) {
+                        if(obj.currentStyle) {
+                                    return obj.currentStyle[attr];
+                        } else {
+                                    return getComputedStyle(obj, false)[attr];
+                        }
+            }
+            
