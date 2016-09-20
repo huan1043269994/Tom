80cc9b0589edc5808a4f2694dfed680f209e23ac
指尖泛出的繁华 <1043269994@qq.com>
2016-09-20 23:42:37 +0800

    Create stripeTables.js

---
+++ jsDom/ninth/table/stripeTables.js
@@ -0,0 +1,21 @@
+/**
+ * Created by hw on 16/9/20.
+ */
+function stripeTables() {
+    if(!document.getElementsByTagName) return false;
+    var tables = document.getElementsByTagName("table");
+    var odd,rows;
+    for (var i = 0; i < tables.length; i++) {
+        odd = false;
+        rows = tables[i].getElementsByTagName("tr");
+        for(var j = 0; j < rows.length; j++) {
+            if(odd) {
+                rows[j].style.backgroundColor = "#ffc";
+                odd = false;
+            } else {
+                odd = true;
+            }
+        }
+    }
+}
+addLoadEvent(stripeTables);
