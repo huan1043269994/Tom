5358f4e4e80eb06609885d51cd6f8100cec08f5d
指尖泛出的繁华 <1043269994@qq.com>
2016-09-20 23:50:58 +0800

    Create addClass.js

---
+++ jsDom/ninth/table/addClass.js
@@ -0,0 +1,13 @@
+/**
+ * Created by hw on 16/9/20.
+ */
+function addClass(element, value) {
+    if (!element.className) {
+        element.className = value;
+    } else {
+        newClassName = element.className;
+        newClassName += " ";
+        newClassName += value;
+        element.className = newClassName;
+    }
+}
